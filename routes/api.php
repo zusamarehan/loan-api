efb8fa919dc9bcad4e382c73537c3b39ba2d9844
Rehan KU <zrehan286@gmail.com>
2022-09-12 12:05:18 +0530

    missing: forgot to commit the api.php file

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::post('/request', Controllers\Loan\LoanRequestController::class);
         });
         Route::middleware(['admin'])->group(function () {
+            Route::get('/', Controllers\Loan\LoanListController::class);
             Route::post('/{loan}/approve', Controllers\Loan\LoanApproveController::class);
             Route::post('/{loan}/decline', Controllers\Loan\LoanDeclineController::class);
         });
