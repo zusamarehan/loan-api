933a0079ad2e031eee33336f8860a8ea67a95fd6
Rehan KU <zrehan286@gmail.com>
2022-09-12 15:14:41 +0530

    tests: added test for loan request

---
+++ tests/Feature/Loan/LoanRequestTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature\Loan;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoanRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_loan_request_can_be_created_with_correct_inputs()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $amount = 100;
+        $term = 4;
+
+        $response = $this->postJson(route('customer.loan.request'), [
+            'amount' => $amount,
+            'term' => $term,
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonPath('amount', $amount);
+            $response->assertJsonPath('term', $term);
+            $response->assertJsonCount($term, 'repayments');
+            $response->assertJsonPath('user_id', $user->id);
+    }
+
+    public function test_a_loan_request_cannot_be_created_with_incorrect_amount()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $amount = 'one hundred';
+        $term = 4;
+
+        $response = $this->postJson(route('customer.loan.request'), [
+            'amount' => $amount,
+            'term' => $term,
+        ]);
+
+        $this->assertArrayHasKey('errors', $response);
+        $this->assertArrayHasKey('amount', $response['errors']);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_a_loan_request_cannot_be_created_with_incorrect_term()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $amount = 100;
+        $term = '5 term';
+
+        $response = $this->postJson(route('customer.loan.request'), [
+            'amount' => $amount,
+            'term' => $term,
+        ]);
+
+        $this->assertArrayHasKey('errors', $response);
+        $this->assertArrayHasKey('term', $response['errors']);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_a_loan_cannot_be_requested_by_a_admin_user()
+    {
+        $user = User::factory([
+            'is_admin' => 1
+        ])->create();
+
+        $this->actingAs($user);
+
+        $amount = 100;
+        $term = '5';
+
+        $this->postJson(route('customer.loan.request'), [
+            'amount' => $amount,
+            'term' => $term,
+        ])->assertUnauthorized();
+
+    }
+}
